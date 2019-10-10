0eac111e754f636c3d5a9e8143baafa91e56d9af
mjack31 <shark3113@gmail.com>
2019-10-10 22:56:51 +0200

    Setup caliburn.micro

---
+++ ShopManager.DesktopGUI/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using ShopManager.DesktopGUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ShopManager.DesktopGUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
